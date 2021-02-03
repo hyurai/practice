61edbffdfcb73ffcface2932cec81408479665e9
hyurai <yuumanagai0504@icloud.com>
2021-02-03 19:52:51 +0900

    Update classmethod_instancemethod.php

--- classmethod_instancemethod.php
+++ classmethod_instancemethod.php
@@ -1,11 +1,24 @@
 <?php 
 
-class Human{
- function(){
+class Number{
 
-    
+ public function addNumbers1($a,$b,$c,$d,$e){
+     echo $a + $b + $c + $d + $e;
  }
 }
+class deleteNumber{
+  public static function addNumber2($f,$g,$h,$i,$j){
+  echo self.$f + $g + $h + $i + $j;
+  self::addNumber2(20,5,4,3,2);
+}
+
+}
+
+$plusnumber =  new Number();
+$plusnumber.addNumbers(1,2,3,4,5)
+//インスタンスメソッドこちらは一回メソッドをインスタンス化してメソッドを呼び出すことができる
+
+//クラスメソッドこちらはクラスから直接メソッドを呼び出すことができる。その分クラス内のメソッドにselfを付ける必要がある
 
 
 ?>
