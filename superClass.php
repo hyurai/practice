6e809f5ec17da69c79896d264ff387dcae04b8f1
hyurai <yuumanagai0504@icloud.com>
2021-02-06 16:33:14 +0900

    Update superClass.php

--- superClass.php
+++ superClass.php
@@ -3,6 +3,7 @@ class AllNumber{
     public $number = 1;
     public function randnumber(){
         $number = 1;
+        echo $number;
     }
 
 }
@@ -11,10 +12,15 @@ class updateNumber extends AllNumber{
     public function __construct(){
         $number = 2;
         echo $number;
+        //オーバーライド = 継承してクラスでデータを更新すること
     }
 }
 
-$echoNumber = new updateNumber();
+$echoAllNumber = new AllNumber();
+$echoAllNumber->randnumber();
+//スーパークラスの呼び出し
+$echoUpdateNumber = new updateNumber();
+//子クラスの呼び出し
 
 
 
