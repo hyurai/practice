3cc34c4e71c1370ce54768e17536c08718222397
hyurai <yuumanagai0504@icloud.com>
2021-02-06 14:41:24 +0900

    Update construct.php

--- construct.php
+++ construct.php
@@ -3,12 +3,13 @@
 class Human{
 //construct
 public function __construct(){
-    //constructはnewメソッドが使われると自動的に呼び出される。
+    //constructはnewメソッドが使われると自動的に呼び出される。一番最初に呼び出される
  echo '私は神から生まれた';
  
 }
 
 public function __destruct(){
+    //destructもconstructとほぼ一緒である。しかし違う点は一番最後に呼び出される。もうするべき処理がないという時に呼び出される。一番大変だった...
     echo '私は最後の呼ばれる';    
 } 
 public function helloman(){
@@ -19,7 +20,6 @@ public function helloman(){
 $taro =  new Human;
 $taro->helloman();
 echo '私は女性です';
-
 
 
 
