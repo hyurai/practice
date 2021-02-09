f44e8cd86295e14c6683dcff3fab678d39005d70
hyurai <yuumanagai0504@icloud.com>
2021-02-09 16:19:01 +0900

    Update style.php

--- style.php
+++ style.php
@@ -1,7 +1,25 @@
 <?php 
+//array型
 $os = array('Windouws','macOS','Linux');
 //arrayは値が配列でないといけない
-$String = string('私はmacOSを使っています');
+echo $os[0].'は使っていません';
+//String型文字列
+$string = 'macOSを使っています';
+echo $string;
 
+//integer型整数
+$integer1 = 2;
+$integer2 = 1;
+echo $integer1 + $integer2;
+
+//boolaen型false or trueしか入らない。
+$boolean = true;
+if ($boolean =! false){
+echo 'これはtureだ';
+
+}
+//Date型今日の日付が表示される
+echo date('Y年m月d日').'<br>';
+echo date('Y年m月d日　H時i分s秒');
 
 ?>
